20146e5a42797418c526241846b50a64b7f2b8ca
Muhammad Ahmad <m.ahmad@cern.ch>
2025-09-19 14:40:14 +0200

    AnaCache changes for stitching

--- AnaProd/anaCacheProducer.py
+++ AnaProd/anaCacheProducer.py
@@ -4,31 +4,59 @@ import os
 import sys
 import yaml
 import ROOT
-
+from array import array
 
 if __name__ == "__main__":
     sys.path.append(os.environ["ANALYSIS_PATH"])
 
 
-def computeAnaCache(file_lists, global_params, generator, range=None):
+def computeAnaCache(file_lists, global_params, generator, event_range=None):
     from Corrections.Corrections import Corrections
     from Corrections.CorrectionsCore import central, getScales, getSystName
     from Corrections.pu import puWeightProducer
-
     start_time = datetime.datetime.now()
     Corrections.initializeGlobal(
         global_params, sample_name=None, isData=False, load_corr_lib=True
     )
-    anaCache = {"denominator": {}}
+    DY_to_stitch = global_params["DY_stitching_enable"]
+
+    if DY_to_stitch:
+        var1 = global_params["DY_stitch_variables"]["var1"]
+        var2 = global_params["DY_stitch_variables"]["var2"]
+        var1_expr = var1["expression"]
+        var2_expr = var2["expression"]
+        var1_bins = var1["bins"]
+        var2_bins = var2["bins"]
+        anaCache = {
+            "denominator": {},
+            "DY_stitching": {
+                "evts_per_bin": {}
+            }
+        }
+    else:
+        anaCache = {"denominator": {}}
     sources = [central]
     if "pu" in Corrections.getGlobal().to_apply:
         sources += puWeightProducer.uncSource
 
     for tree, file_list in file_lists.items():
         df = ROOT.RDataFrame(tree, file_list)
-        if range is not None:
-            df = df.Range(range)
+        if event_range is not None:
+            df = df.Range(event_range)
         df, syst_names = Corrections.getGlobal().getDenominator(df, sources, generator)
+        # Bin-wise histogram for central/Nominal
+        if DY_to_stitch:
+            h2d = df.Histo2D(
+                ("h2d", "", len(var1_bins)-1, array('d', var1_bins), len(var2_bins)-1, array('d', var2_bins)),
+                var1_expr, var2_expr
+            )
+            for i in range(1, len(var1_bins)):
+                for j in range(1, len(var2_bins)):
+                    bin_key = f"{var1_expr}_{var1_bins[i-1]}_{var1_bins[i]}__{var2_expr}_{var2_bins[j-1]}_{var2_bins[j]}"
+                    bin_content = h2d.GetBinContent(i, j)
+                    anaCache["DY_stitching"]["evts_per_bin"][bin_key] = (
+                        anaCache["DY_stitching"]["evts_per_bin"].get(bin_key, 0.0) + bin_content
+                    )
         for source in sources:
             if source not in anaCache["denominator"]:
                 anaCache["denominator"][source] = {}
@@ -75,6 +103,16 @@ def addAnaCaches(*anaCaches):
                             f"addAnaCaches: {source}/{scale} not found in first cache"
                         )
                 anaCacheSum["denominator"][source][scale] += value
+        # Merge stitching
+        if "DY_stitching" in anaCache:
+            if "DY_stitching" not in anaCacheSum:
+                anaCacheSum["DY_stitching"] = {
+                    "evts_per_bin": {}
+                }
+            for bin_key, value in anaCache["DY_stitching"]["evts_per_bin"].items():
+                anaCacheSum["DY_stitching"]["evts_per_bin"][bin_key] = (
+                    anaCacheSum["DY_stitching"]["evts_per_bin"].get(bin_key, 0.0) + value
+                )
         anaCacheSum["runtime"] += anaCache["runtime"]
     return anaCacheSum
 
@@ -94,11 +132,11 @@ if __name__ == "__main__":
     from FLAF.Common.Utilities import DeserializeObjectFromString
 
     input_files = args.input_files.split(",")
-    global_params = DeserializeObjectFromString(args.global_params)
 
+    global_params = DeserializeObjectFromString(args.global_params)
     file_lists = create_filelists(input_files)
     anaCache = computeAnaCache(
-        file_lists, global_params, args.generator_name, range=args.n_events
+        file_lists, global_params, args.generator_name, event_range=args.n_events
     )
     if args.verbose > 0:
         print(json.dumps(anaCache))
