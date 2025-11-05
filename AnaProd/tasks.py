7f7caa5b460bcb3f21f30ce485dcc6ba216f195a
Muhammad Ahmad <ahmad3213@users.noreply.github.com>
2025-11-05 23:50:01 +0100

    Update tasks.py
    increasing nEvents in integration test from 1000 to 10000. We think it could be the reason for crash of following 
    
    https://gitlab.cern.ch/cms-flaf/flaf_integration/-/jobs/64123272

--- AnaProd/tasks.py
+++ AnaProd/tasks.py
@@ -353,7 +353,7 @@ class AnaTupleTask(Task, HTCondorWorkflow, law.LocalWorkflow):
 
                 centralFileName = os.path.basename(local_input.path)
                 if self.test:
-                    anatuple_cmd.extend(["--nEvents", "1000"])
+                    anatuple_cmd.extend(["--nEvents", "10000"])
                 # ps_call(anatuple_cmd, verbose=1) # this will be uncommented when the anatuple producer will be fully independent on cmsEnv.
                 ps_call(anatuple_cmd, env=self.cmssw_env, verbose=1)
 
